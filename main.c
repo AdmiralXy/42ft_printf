1a4e4a67ef3e38a37da805bd88e9269b20987450
AdmiralXy <gameproblem80@gmail.com>
2021-05-27 00:31:34 +0300

    add new h to makefile

--- main.c
+++ main.c
@@ -3,9 +3,6 @@
 
 int	main(void)
 {
-	int ncm_p;
-
-	ncm_p = 0x20F;
-	printf("\n%d\n\n", printf("%-0.2d", ncm_p));
-	printf("\n%d", ft_printf("%-0.2d", ncm_p));
+	printf("\n%d\n\n", printf("%0.*i test\n", 5, 0));
+	printf("\n%d", ft_printf("%0.*i test\n", 5, 0));
 }
